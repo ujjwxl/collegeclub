052c1959591a196a2b5651f50baf69bd7c4fff3f
agrawalaman775 <agrawalaman977@gmail.com>
2024-03-09 13:10:16 +0530

    commit 3
    move pages to src

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Home } from '../pages/Home/Home'
+import { Home } from './pages/Home/Home';
 import './App.css'
 
 function App() {
