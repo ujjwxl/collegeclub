40c3e11a0bce03165a2b06413e174c220b362f98
agrawalaman775 <agrawalaman977@gmail.com>
2024-03-09 14:33:32 +0530

    commit 7
    added images and transformed navbar

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import home1 from '../../assets/home-1.jpg';
 
 export const Home = () => {
   return (
     <>
       <Navbar/>
-      <div>Home</div>
+      <div>
+        <img src={home1} alt="" className='home1-img'></img>
+      </div>
     </>
   )
 }
