9c0e0519d71b878a1dbcdf7fe93240af5f1c8579
ujjwxl <ujjwalk1408@gmail.com>
2024-03-11 21:26:25 +0530

    commit 20
    made navbar and landing section responsive on mobile

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -9,15 +9,39 @@ import './Navbar.css'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen]= useState('false');
-  
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setIsMenuOpen(prevState => !prevState);
+  }
+
   return (
     <div className='navbar'>
       <div className="navbar-logo">
-        <img src={collegeClubLogo} alt="" className='navbar-brand-logo'/>
-        <h2>COLLEGE<br/><span>CLUB</span></h2>
+        <img src={collegeClubLogo} alt="" className='navbar-brand-logo' />
+        <h2>COLLEGE<br /><span>CLUB</span></h2>
       </div>
-      <img src={menu} alt='menu' className='navbar-menu-icon' onClick={handleToggleMenu}/> 
+      <img src={menu} alt='menu' className='navbar-menu-icon' onClick={handleToggleMenu} />
+
+      {isMenuOpen && (
+        <div className='mobile-menu'>
+          <div className='navbar-links-mobile'>
+            <Link to="">Products</Link>
+            <Link to="">College</Link>
+            <Link to="">Courses</Link>
+            <Link to="">Exam</Link>
+            <Link to="">Career</Link>
+            <Link to="">Help?</Link>
+          </div>
+
+          <div className='navbar-right-mobile'>
+            <Link to="">Onboarding</Link>
+            <Link to=""><img src={compare} alt='' className='navbar-bell'></img></Link>
+            <Link to=""><img src={notification} alt='' className='navbar-bell'></img></Link>
+            <Link to=""><img src={signout} alt='' className='navbar-bell'></img></Link>
+          </div>
+        </div>
+      )}
 
       <div className='navbar-links'>
         <Link to=""> Products</Link>
