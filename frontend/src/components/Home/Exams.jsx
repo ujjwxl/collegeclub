1a3d633991871fa938325783e00de2ec0426152e
ujjwxl <ujjwalk1408@gmail.com>
2024-03-12 19:26:17 +0530

    commit 22
    added slider

--- frontend/src/components/Home/Exams.jsx
+++ frontend/src/components/Home/Exams.jsx
@@ -7,12 +7,57 @@ import "slick-carousel/slick/slick-theme.css";
 
 const Exams = () => {
 
+  // const settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 6,
+  //   slidesToScroll: 1,
+  // };
+
+  // const settings = {
+  //   slidesToShow: 6,
+  //   slidesToScroll: 1,
+  //   infinite: true,
+  //   speed: 500,
+  //   dots: false,
+  //   arrows: true,
+  //   responsive: [
+  //     {
+  //       breakpoint: 768,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1,
+  //         initialSlide: 1,
+  //       },
+  //     },
+  //   ],
+  // };
+
   const settings = {
-    dots: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1024, // Tablet size breakpoint
+        settings: {
+          slidesToShow: 3, // Show 3 circles on tablets
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+    ],
   };
 
   return (
@@ -21,14 +66,14 @@ const Exams = () => {
         <div className='home-two-overlay'>
           <h2 className='home-two-h2'>TRENDING ENTRANCE EXAMS</h2>
           <p className='home-two-p'>Get information and updates on competitive exams, dates, online classes<br></br> and preparation tips.</p>
-          <Slider {...settings}>
+          <Slider {...settings} className='home-exam-slider'>
           {/* <div class="circle-row"> */}
-            <div class="circle">CLAT</div>
-            <div class="circle">JEE</div>
-            <div class="circle">NEET</div>
-            <div class="circle">CAT</div>
-            <div class="circle">NIFT</div>
-            <div class="circle">ICAR</div>
+            <div class="circle"><h3>CLAT</h3></div>
+            <div class="circle"><h3>JEE</h3></div>
+            <div class="circle"><h3>NEET</h3></div>
+            <div class="circle"><h3>CAT</h3></div>
+            <div class="circle"><h3>NIFT</h3></div>
+            <div class="circle"><h3>ICAR</h3></div>
             {/* <div class="circle">CUET</div> */}
           {/* </div> */}
           </Slider>
