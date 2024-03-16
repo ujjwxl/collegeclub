0865994aec7ab5e0653958d7ec9d4616f398fb8a
ujjwxl <ujjwalk1408@gmail.com>
2024-03-16 14:55:03 +0530

    commit 36
    completed signup page

--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -7,14 +7,51 @@ import './Signup.css'
 const Signup = () => {
     return (
         <div>
-            <Navbar/>
-            <img src={backgroundImage} alt="" className='home1-img'/>
+            <Navbar />
+            <img src={backgroundImage} alt="" className='home1-img' />
 
             <div className="signup-overlay">
-                
+                <div className="signup-form">
+
+
+                    <form action="" className='signup-form-container'>
+                        <div className="signup-form-left">
+                            <h2>Signup</h2>
+                            <h2>for New Account</h2>
+                            <select name="type">
+                                <option value="" disabled selected>Select account type</option>
+                                <option value="college">College</option>
+                                <option value="company">Company</option>
+                                <option value="ccAmbassador">CC Ambassador</option>
+                            </select> <br />
+
+                            <input type="text" name="organization" placeholder='Enter organization name' /> <br />
+                            <input type="text" name="organization" placeholder='Enter organization name' /> <br />
+                            <input type="text" name="organization" placeholder='Enter organization name' /> <br />
+                            <input type="text" name="organization" placeholder='Enter organization name' />
+                        </div>
+
+                        <div className="signup-form-right">
+                            <input type="text" name="organization" placeholder='Enter organization name' /> <br />
+                            <input type="text" name="organization" placeholder='Enter organization name' /> <br />
+                            <input type="text" name="organization" placeholder='Enter organization name' /> <br />
+                            <input type="text" name="organization" placeholder='Enter organization name' />
+
+                            <label class="checkbox-label">
+                                <input type="checkbox" name="agreeToTerms" />
+                                <p>By registering I agree to Terms & Privacy policy of <br/> the website.</p>
+                            </label> <br />
+
+                            <button>Create Account</button>
+
+                            <p className='already-have-account'>Already have an account? Login from here</p>
+                        </div>
+                    </form>
+
+                </div>
             </div>
 
-            <Footer/>
+            <Footer />
         </div>
     )
 }
