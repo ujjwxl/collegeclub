67f8885bbe7eb7d5aaf80c27e0ac9cb1d913114b
ujjwxl <ujjwalk1408@gmail.com>
2024-03-16 13:03:27 +0530

    commit 33
    created signup and login routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home/Home';
+import Signup from './pages/Signup/Signup';
 import './App.css'
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
       <Router>
         <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/register" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
         </Routes>
       </Router>
     </>
---
+++ frontend/src/pages/Signup/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import backgroundImage from '../../assets/home-1.jpg'
+
+const Signup = () => {
+  return (
+    <div>
+      <img src={backgroundImage} alt="" />
+    </div>
+  )
+}
+
+export default Signup
