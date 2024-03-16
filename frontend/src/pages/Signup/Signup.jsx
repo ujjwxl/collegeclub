cd611a2619ec04c65e55bbe78123e3fb8dfb75bf
ujjwxl <ujjwalk1408@gmail.com>
2024-03-16 13:16:03 +0530

    commit 34
    created structure for signup page

--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import backgroundImage from '../../assets/home-1.jpg'
+import Navbar from '../../components/Navbar/Navbar'
+import Footer from '../../components/Home/Footer'
+import './Signup.css'
 
 const Signup = () => {
-  return (
-    <div>
-      <img src={backgroundImage} alt="" />
-    </div>
-  )
+    return (
+        <div>
+            <Navbar/>
+            <img src={backgroundImage} alt="" className='home1-img'/>
+
+            <div className="signup-overlay">
+                
+            </div>
+
+            <Footer/>
+        </div>
+    )
 }
 
 export default Signup
