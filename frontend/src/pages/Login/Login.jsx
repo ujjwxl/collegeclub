24d7e998d8050b6cd31529099864a0509f675228
agrawalaman775 <agrawalaman977@gmail.com>
2024-03-17 12:57:49 +0530

    commit 39
    Firebase login signup

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar.jsx";
 import Footer from "../../components/Home/Footer.jsx";
 import backgroundImage from "../../assets/home-1.jpg";
 import logo from "../../assets/collegeclub-logo.png";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase.js";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(error);
+      });
+  };
   return (
     <div>
       <Navbar />
@@ -16,23 +35,33 @@ const Login = () => {
         <div className="login-component">
           <img src={logo} alt="Club Logo" className="CC-logo"></img>
           <div className="login-box">
-            <p className="header">Signin or Signup<br></br> to Continue</p>
-            <div className="input-container">
-              <input type="text" id="username" placeholder="Email id" />
-            </div>
-            <div className="input-container">
-              <input type="password" id="password" placeholder="Password" />
-            </div>
-            <div className="remember-forgot">
-              <div className="checkbox-comp">
-                <input type="checkbox"></input>
-                <label>Remember me</label>
+            <p className="header">
+              Signin or Signup<br></br> to Continue
+            </p>
+            <form onSubmit={handleLogin}>
+              <div className="input-container">
+                <input type="email" id="username" placeholder="Email id" onChange={e=>setEmail(e.target.value)}/>
               </div>
-              <a href="#">Forgot Password?</a>
-            </div>
-            <button className="login-btn">Login</button>
-            <p className="no-account">No account? <Link to={'/register'}>Create one!</Link></p>
-            <Link className="terms">Privacy Policy</Link><span> | </span><Link className="terms">Terms</Link>
+              <div className="input-container">
+                <input type="password" id="password" placeholder="Password" onChange={e=>setPassword(e.target.value)}/>
+              </div>
+              <div className="remember-forgot">
+                <div className="checkbox-comp">
+                  <input type="checkbox"></input>
+                  <label>Remember me</label>
+                </div>
+                <a href="#">Forgot Password?</a>
+              </div>
+              <button className="login-btn" type="submit">
+                Login
+              </button>
+            </form>
+            <p className="no-account">
+              No account? <Link to={"/register"}>Create one!</Link>
+            </p>
+            <Link className="terms">Privacy Policy</Link>
+            <span> | </span>
+            <Link className="terms">Terms</Link>
           </div>
         </div>
       </div>
--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -1,60 +1,124 @@
-import React from 'react'
-import backgroundImage from '../../assets/home-1.jpg'
-import Navbar from '../../components/Navbar/Navbar'
-import Footer from '../../components/Home/Footer'
-import './Signup.css'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import backgroundImage from "../../assets/home-1.jpg";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Home/Footer";
+import "./Signup.css";
+import { Link } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase.js";
 
 const Signup = () => {
-    return (
-        <div>
-            <Navbar />
-            <img src={backgroundImage} alt="" className='home1-img' />
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+const handleSubmit= (e) => {
+    e.preventDefault();
 
-            <div className="signup-overlay">
-                <div className="signup-form">
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+      console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(error);
+      // ..
+    });
+}
+  return (
+    <div>
+      <Navbar />
+      <img src={backgroundImage} alt="" className="home1-img" />
 
-
-                    <form action="" className='signup-form-container'>
-                        <div className="signup-form-left">
-                            <h2>Signup</h2>
-                            <h2>for New Account</h2>
-                            <select name="type">
-                                <option value="" disabled selected>Select account type</option>
-                                <option value="college">College</option>
-                                <option value="company">Company</option>
-                                <option value="ccAmbassador">CC Ambassador</option>
-                            </select> <br />
-
-                            <input type="text" name="organization" placeholder='Enter organization name' /> <br />
-                            <input type="text" name="organization" placeholder='Enter organization name' /> <br />
-                            <input type="text" name="organization" placeholder='Enter organization name' /> <br />
-                            <input type="text" name="organization" placeholder='Enter organization name' />
-                        </div>
-
-                        <div className="signup-form-right">
-                            <input type="text" name="organization" placeholder='Enter organization name' /> <br />
-                            <input type="text" name="organization" placeholder='Enter organization name' /> <br />
-                            <input type="text" name="organization" placeholder='Enter organization name' /> <br />
-                            <input type="text" name="organization" placeholder='Enter organization name' />
-
-                            <label class="checkbox-label">
-                                <input type="checkbox" name="agreeToTerms" />
-                                <p>By registering I agree to Terms & Privacy policy of <br/> the website.</p>
-                            </label> <br />
-
-                            <button>Create Account</button>
-
-                            <p className='already-have-account'>Already have an account? <Link to={'/login'}>Login</Link> from here</p>
-                        </div>
-                    </form>
-
-                </div>
+      <div className="signup-overlay">
+        <div className="signup-form">
+          <form onSubmit={handleSubmit} className="signup-form-container">
+            <div className="signup-form-left">
+              <h2>Signup</h2>
+              <h2>for New Account</h2>
+              <select name="type">
+                <option value="" disabled selected>
+                  Select account type
+                </option>
+                <option value="college">College</option>
+                <option value="company">Company</option>
+                <option value="ccAmbassador">CC Ambassador</option>
+              </select>{" "}
+              <br />
+              <input
+                type="text"
+                name="organization"
+                placeholder="Enter organization name"
+              />{" "}
+              <br />
+              <input
+                type="text"
+                name="organization"
+                placeholder="Enter organization name"
+              />{" "}
+              <br />
+              <input
+                type="text"
+                name="organization"
+                placeholder="Enter organization name"
+              />{" "}
+              <br />
+              <input
+                type="text"
+                name="organization"
+                placeholder="Enter organization name"
+              />
             </div>
 
-            <Footer />
+            <div className="signup-form-right">
+              <input
+                type="text"
+                name="organization"
+                placeholder="Enter organization name"
+              />{" "}
+              <br />
+              <input
+                type="text"
+                name="organization"
+                placeholder="Enter organization name"
+              />{" "}
+              <br />
+              <input
+                type="email"
+                name="organization"
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />{" "}
+              <br />
+              <input
+                type="password"
+                name="organization"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <label class="checkbox-label">
+                <input type="checkbox" name="agreeToTerms" />
+                <p>
+                  By registering I agree to Terms & Privacy policy of <br /> the
+                  website.
+                </p>
+              </label>{" "}
+              <br />
+              <button type="submit">Create Account</button>
+              <p className="already-have-account">
+                Already have an account? <Link to={"/login"}>Login</Link> from
+                here
+              </p>
+            </div>
+          </form>
         </div>
-    )
-}
+      </div>
 
-export default Signup
+      <Footer />
+    </div>
+  );
+};
+
+export default Signup;
