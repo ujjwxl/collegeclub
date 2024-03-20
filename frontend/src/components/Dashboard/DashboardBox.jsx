0c3e759fb978828b59c91ed74b13443e937c4e09
agrawalaman775 <agrawalaman977@gmail.com>
2024-03-20 11:05:57 +0530

    commit 46
    design dashboard

--- frontend/src/components/Dashboard/DashboardBox.jsx
+++ frontend/src/components/Dashboard/DashboardBox.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import './DashboardBox.css'
+import React from "react";
+import "./DashboardBox.css";
 
 const DashboardBox = () => {
   return (
-    <div className='dashboard-box'>
-      <h2>You have signed up successfully!</h2>
-      <h3>Continue to payment to verify the account</h3>
+    <div className="dashboard-box">
+      <div className="dashboard-box-container">
+        <h2>You have signed up successfully!</h2>
+        <h3>Continue to payment to verify the account</h3>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardBox
+export default DashboardBox;
--- frontend/src/components/Dashboard/DashboardMenu.jsx
+++ frontend/src/components/Dashboard/DashboardMenu.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import defaultImage from '../../assets/test-dp.jpg'
-import testIcon from '../../assets/business-icon.png'
-import './DashboardMenu.css'
+import React from "react";
+import defaultImage from "../../assets/test-dp.jpg";
+import testIcon from "../../assets/business-icon.png";
+import dashboardIcon from "../../assets/dashboard.png";
+import applicationIcon from "../../assets/edit.png";
+import notificationIcon from "../../assets/notification.png";
+import helpIcon from "../../assets/help.png";
+import logoutIcon from "../../assets/close.png";
+import "./DashboardMenu.css";
 
 const DashboardMenu = () => {
   return (
-    <div className='dashboard-menu'>
-
+    <div className="dashboard-menu">
       <div className="dashboard-menu-top">
         <img src={defaultImage} alt="" />
         <div className="dashboard-menu-top-right">
-          <h2>NIFT, PATNA</h2>
+          <h2>
+            NIFT, PATNA <span>✅</span>
+          </h2>
           <h3>Vijay Prakash</h3>
           <h4>testingcollege@collegeclub.io</h4>
           <h3>COLLEGE</h3>
@@ -28,16 +34,36 @@ const DashboardMenu = () => {
       <hr />
 
       <div className="dashboard-menu-bottom">
-        <ul>
-          <li>Dashboard</li>
-          <li>Application</li>
-          <li>Notification</li>
-          <li>Help</li>
-        </ul>
+        <div className="dashboard-menu-bottom-options">
+          <img src={dashboardIcon} alt="" />
+          <h3>Dashboard</h3>
+        </div>
+        <hr />
+        <div className="dashboard-menu-bottom-options">
+          <img src={applicationIcon} alt="" />
+
+          <h3>Application</h3>
+        </div>
+        <hr />
+        <div className="dashboard-menu-bottom-options">
+          <img src={notificationIcon} alt="" />
+
+          <h3>Notifications</h3>
+        </div>
+        <hr />
+        <div className="dashboard-menu-bottom-options">
+          <img src={helpIcon} alt="" />
+
+          <h3>Help</h3>
+        </div>
+        <hr />
       </div>
-
+      <div className="logout">
+        <img src={logoutIcon} alt="" />
+        <h3>Logout</h3>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardMenu
+export default DashboardMenu;
