7e022c32b7e03f724b79e5cb4e36d02c6344077e
agrawalaman775 <agrawalaman977@gmail.com>
2024-04-07 15:07:37 +0530

    commit 81
    colleges page

--- frontend/src/pages/College/College.jsx
+++ frontend/src/pages/College/College.jsx
@@ -1,20 +1,20 @@
-import React, { useState, useEffect, cloneElement } from 'react'
-import { useNavigate } from 'react-router-dom'
-import Navbar from '../../components/Navbar/Navbar'
-import Footer from '../../components/Home/Footer'
-import backgroundImage from '../../assets/home-1.jpg'
-import defaultImage from '../../assets/test-dp.jpg'
-import axios from 'axios'
-import './College.css'
+import React, { useState, useEffect, cloneElement } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
+import location from  '../../assets/location.png';
+import Footer from "../../components/Home/Footer";
+import backgroundImage from "../../assets/home-1.jpg";
+import defaultImage from "../../assets/test-dp.jpg";
+import axios from "axios";
+import "./College.css";
 
 const College = () => {
-
   const [collegeData, setCollegeData] = useState(null);
   const [filters, setFilters] = useState({
     instituteType: [],
     degree: [],
     studyMode: [],
-    state: []
+    state: [],
   });
 
   const navigate = useNavigate();
@@ -22,10 +22,10 @@ const College = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/auth/college');
+        const response = await axios.get("http://localhost:5000/auth/college");
         setCollegeData(response.data);
       } catch (error) {
-        console.error('Error fetching college data:', error.message);
+        console.error("Error fetching college data:", error.message);
       }
     };
 
@@ -37,7 +37,9 @@ const College = () => {
   const handleFilterChange = (filterType, value) => {
     const updatedFilters = { ...filters };
     if (updatedFilters[filterType].includes(value)) {
-      updatedFilters[filterType] = updatedFilters[filterType].filter(f => f !== value);
+      updatedFilters[filterType] = updatedFilters[filterType].filter(
+        (f) => f !== value
+      );
     } else {
       updatedFilters[filterType] = [...updatedFilters[filterType], value];
     }
@@ -45,26 +47,32 @@ const College = () => {
   };
 
   const filteredColleges = collegeData
-    ? collegeData.filter(college => {
-      return (
-        (filters.instituteType.length === 0 || filters.instituteType.includes(college.instituteType)) &&
-        // (filters.degree.length === 0 || filters.degree.includes(college.degree)) &&
-        (filters.degree.length === 0 || (Array.isArray(college.selectedCourses) && college.selectedCourses.some(course => filters.degree.includes(course)))) &&
-        (filters.studyMode.length === 0 || filters.studyMode.includes(college.studyMode)) &&
-        (filters.state.length === 0 || filters.state.includes(college.state))
-      );
-    })
+    ? collegeData.filter((college) => {
+        return (
+          (filters.instituteType.length === 0 ||
+            filters.instituteType.includes(college.instituteType)) &&
+          // (filters.degree.length === 0 || filters.degree.includes(college.degree)) &&
+          (filters.degree.length === 0 ||
+            (Array.isArray(college.selectedCourses) &&
+              college.selectedCourses.some((course) =>
+                filters.degree.includes(course)
+              ))) &&
+          (filters.studyMode.length === 0 ||
+            filters.studyMode.includes(college.studyMode)) &&
+          (filters.state.length === 0 || filters.state.includes(college.state))
+        );
+      })
     : [];
 
   const openCollegeDetails = (collegeId) => {
     navigate(`/college/${collegeId}`);
-  }
+  };
 
   return (
     <>
       <Navbar />
-      <img src={backgroundImage} alt="" className='home1-img' />
-      <div className='colleges-overlay colleges-container'>
+      <img src={backgroundImage} alt="" className="home1-img" />
+      <div className="colleges-overlay colleges-container">
         <div className="colleges-display-box">
           {/* <div className="colleges-display-box-filter">
             <h3>Filters</h3>
@@ -82,16 +90,18 @@ const College = () => {
           <div className="colleges-display-box-filter">
             <h3>Filters</h3>
             <h4>Institute Type</h4>
-            {['Central', 'State', 'Deemed', 'Private', 'Autonomous'].map(type => (
-              <label key={type}>
-                <input
-                  type="checkbox"
-                  checked={filters.instituteType.includes(type)}
-                  onChange={() => handleFilterChange('instituteType', type)}
-                />{' '}
-                {type} <br />
-              </label>
-            ))}
+            {["Central", "State", "Deemed", "Private", "Autonomous"].map(
+              (type) => (
+                <label key={type}>
+                  <input
+                    type="checkbox"
+                    checked={filters.instituteType.includes(type)}
+                    onChange={() => handleFilterChange("instituteType", type)}
+                  />{" "}
+                  {type} <br />
+                </label>
+              )
+            )}
             {/* <label htmlFor="">
               <input type="checkbox" /> Central/Union
             </label> <br />
@@ -126,13 +136,19 @@ const College = () => {
             </label> */}
 
             <h4>Degree</h4>
-            {['Engineering', 'Medical', 'Management', 'Law', 'Fashion Technology'].map(degree => (
+            {[
+              "Engineering",
+              "Medical",
+              "Management",
+              "Law",
+              "Fashion Technology",
+            ].map((degree) => (
               <label key={degree}>
                 <input
                   type="checkbox"
                   checked={filters.degree.includes(degree)}
-                  onChange={() => handleFilterChange('degree', degree)}
-                />{' '}
+                  onChange={() => handleFilterChange("degree", degree)}
+                />{" "}
                 {degree} <br />
               </label>
             ))}
@@ -146,13 +162,13 @@ const College = () => {
             </label> */}
 
             <h4>Study Mode</h4>
-            {['Regular', 'Distance'].map(mode => (
+            {["Regular", "Distance"].map((mode) => (
               <label key={mode}>
                 <input
                   type="checkbox"
                   checked={filters.studyMode.includes(mode)}
-                  onChange={() => handleFilterChange('studyMode', mode)}
-                />{' '}
+                  onChange={() => handleFilterChange("studyMode", mode)}
+                />{" "}
                 {mode} <br />
               </label>
             ))}
@@ -172,13 +188,13 @@ const College = () => {
             </label> <br /> */}
 
             <h4>State</h4>
-            {['Bihar', 'Delhi', 'Maharashtra', 'Gujarat'].map(state => (
+            {["Bihar", "Delhi", "Maharashtra", "Gujarat"].map((state) => (
               <label key={state}>
                 <input
                   type="checkbox"
                   checked={filters.state.includes(state)}
-                  onChange={() => handleFilterChange('state', state)}
-                />{' '}
+                  onChange={() => handleFilterChange("state", state)}
+                />{" "}
                 {state} <br />
               </label>
             ))}
@@ -213,26 +229,44 @@ const College = () => {
           ))} */}
 
           <div className="colleges-display-box-list">
-            {collegeData && filteredColleges.map((college, index) => (
-              <div className="colleges-display-box-item" key={index} onClick={() => openCollegeDetails(college.userId)}>
-                <img src={college.profilePicture ? college.profilePicture : defaultImage} alt="" />
-                <div className="colleges-display-box-item-details">
-                  <h3>{college.organizationName}</h3>
-                  {/* <h3>Birla Institute of Technology, Mesra</h3> */}
+            {collegeData &&
+              filteredColleges.map((college, index) => (
+                <div
+                  className="colleges-display-box-item"
+                  key={index}
+                  onClick={() => openCollegeDetails(college.userId)}
+                >
+                  <img
+                    src={
+                      college.profilePicture
+                        ? college.profilePicture
+                        : defaultImage
+                    }
+                    alt=""
+                  />
+                  <div className="colleges-display-box-item-details">
+                    <div className="list-college-details-page">
+                      <div className="list-college-name">
+                        <h3>{college.organizationName}</h3>
+                      </div>
+                      <div className="list-college-details">
+                      <img src={location}></img>
+                <h4>{college && college.district}, {college && college.state}</h4>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="colleges-display-box-item-details-two">
+                    <h3>{college.email}</h3>
+                    <h3>{college.contactNumber}</h3>
+                  </div>
                 </div>
-                <div className="colleges-display-box-item-details-two">
-                  <h3>{college.email}</h3>
-                  <h3>{college.contactNumber}</h3>
-                </div>
-              </div>
-            ))}
+              ))}
           </div>
-
         </div>
       </div>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default College
+export default College;
--- frontend/src/pages/CollegeDetails/CollegeDetails.jsx
+++ frontend/src/pages/CollegeDetails/CollegeDetails.jsx
@@ -1,73 +1,11 @@
-// import React, { cloneElement, useEffect, useState } from 'react'
-// import { useParams } from 'react-router-dom'
-// import axios from 'axios'
-// import backgroundImage from '../../assets/home-1.jpg'
-// import DetailsFormComponent from '../../components/Forms/DetailsFormComponent'
-// import Navbar from '../../components/Navbar/Navbar'
-// import Footer from '../../components/Home/Footer'
-// import './CollegeDetails.css'
-
-// const CollegeDetails = () => {
-
-//   const { collegeId } = useParams();
-//   const [collegeData, setCollegeData] = useState(null);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await axios.get(`http://localhost:5000/auth/user/${collegeId}`);
-//         setCollegeData(response.data);
-//       } catch (error) {
-//         console.error('Error fetching college data:', error.message);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <div>
-//       <>
-//         <Navbar />
-//         {/* <img src={backgroundImage} alt="" className='home1-img' /> */}
-//         <img src={backgroundImage} alt="" className='details-form-background-image' />
-//         <div className='dashboard-overlay dashboard-container details-form-overlay'>
-//           <div className="college-details-page">
-//             <div className="college-details-page-header">
-//               <img src={collegeData && collegeData.profilePicture} alt="" />
-//               <h2>{collegeData && collegeData.organizationName}</h2>
-//             </div>
-
-//             <h3>About College</h3>
-//             <h4>{collegeData && collegeData.aboutCollege}</h4>
-//             <hr />
-
-//             <h3>Admission Process</h3>
-//             <h4 className='admission-process'>{collegeData && collegeData.admissionProcess}</h4>
-//             <hr />
-
-//             <div className="college-details-page-gallery">
-//               <h3>Gallery</h3>
-//               <img src={collegeData && collegeData.galleryImages[0]} alt="" />
-//               <img src={collegeData && collegeData.galleryImages[1]} alt="" />
-//               <img src={collegeData && collegeData.galleryImages[2]} alt="" />
-//               <img src={collegeData && collegeData.galleryImages[3]} alt="" />
-//               <img src={collegeData && collegeData.galleryImages[4]} alt="" />
-//             </div>
-
-//           </div>
-//         </div>
-//         <Footer />
-//       </>
-//     </div>
-//   )
-// }
-
-// export default CollegeDetails
 import React, { useEffect, useState, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import backgroundImage from '../../assets/home-1.jpg';
+import location from  '../../assets/location.png';
+import bookmark from  '../../assets/bookmark.png';
+import pin from  '../../assets/pin.png';
+import star from  '../../assets/star.png';
 import DetailsFormComponent from '../../components/Forms/DetailsFormComponent';
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Home/Footer';
@@ -112,14 +50,21 @@ const CollegeDetails = () => {
       <div className="dashboard-overlay dashboard-container details-form-overlay">
         <div className="college-details-page">
           <div className="college-details-page-header">
-            <img src={collegeData && collegeData.profilePicture} alt="" />
+            <img src={collegeData && collegeData.profilePicture} alt="" className='college-logo' />
             <div className="college-details-page-header-details">
               <div className="college-name">
                 <h2>{collegeData && collegeData.organizationName}</h2>
                 <h2>({collegeData && collegeData.instituteType} University)</h2>
               </div>
               <div className="college-details">
-                <h4>{collegeData && collegeData.district}</h4>
+                <img src={location}></img>
+                <h4>{collegeData && collegeData.district}, {collegeData && collegeData.state}</h4>
+                <img src={bookmark}></img>
+                <h4>{collegeData && collegeData.approvedBy}</h4>
+                <img src={pin}></img>
+                <h4>Estd {collegeData && collegeData.foundedYear}</h4>
+                <img src={star}></img>
+                <h4>{collegeData && collegeData.instituteType}</h4>
               </div>
             </div>
           </div>
