1598fae2a1198552faba4ec5ae168628d1a45258
ujjwxl <ujjwalk1408@gmail.com>
2024-04-13 09:36:40 +0530

    commit 87
    displaying logged in user on navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 import collegeClubLogo from '../../assets/collegeclub-logo.png'
 import notification from '../../assets/notification.png'
 import signout from '../../assets/close.png'
@@ -9,11 +10,35 @@ import './Navbar.css'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [userData, setUserData] = useState(null);
+  const userId = sessionStorage.getItem('id');
+  const isLoggedIn = userId != null;
+
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      if (!userId) {
+        return;
+      }
+
+      try {
+        const response = await axios.get(`http://localhost:5000/auth/user/${userId}`);
+        setUserData(response.data);
+      } catch (error) {
+        console.error("Failed to fetch user data:", error);
+      }
+    };
+
+    fetchUserData();
+  }, [isLoggedIn]);
 
   const handleToggleMenu = () => {
     setIsMenuOpen(prevState => !prevState);
   }
+
+  console.log(userData)
 
   return (
     <div className='navbar'>
@@ -26,38 +51,78 @@ const Navbar = () => {
       {isMenuOpen && (
         <div className='mobile-menu'>
           <div className='navbar-links-mobile'>
-            <Link to="">Products</Link>
-            <Link to="">College</Link>
+            <Link to="/colleges">Colleges</Link>
             <Link to="">Courses</Link>
-            <Link to="">Exam</Link>
+            <Link to="">Exams</Link>
+            <Link to="">Skills</Link>
             <Link to="">Career</Link>
-            <Link to="">Help?</Link>
+            {/* <Link to="">Help?</Link> */}
           </div>
 
-          <div className='navbar-right-mobile'>
+          {/* <div className='navbar-right-mobile'>
             <Link to="">Onboarding</Link>
+            <Link to="">Login</Link>
             <Link to=""><img src={compare} alt='' className='navbar-bell'></img></Link>
             <Link to=""><img src={notification} alt='' className='navbar-bell'></img></Link>
             <Link to=""><img src={signout} alt='' className='navbar-bell'></img></Link>
+          </div> */}
+
+          <div className='navbar-right-mobile'>
+            {/* <Link to='/register'>Onboarding</Link> */}
+            {isLoggedIn ? (
+              <>
+                <div className="navbar-right-mobile-menu">
+                <img src={compare} alt='' className='navbar-bell' />
+                <img src={notification} alt='' className='navbar-bell' />
+                <img src={signout} alt='' className='navbar-bell' />
+                </div>
+                
+                <Link to=''><img src={userData && userData.profilePicture} alt="" className='navbar-profile-picture' /></Link>
+              </>
+            ) : (
+              <>
+                <Link to='/register'>Onboarding</Link>
+                <Link to='/login'>Login</Link>
+              </>
+            )}
           </div>
         </div>
       )}
 
       <div className='navbar-links'>
-        <Link to=""> Products</Link>
-        <Link to="/colleges">College</Link>
+        <Link to="/colleges">Colleges</Link>
         <Link to="">Courses</Link>
-        <Link to="">Exam</Link>
+        <Link to="">Exams</Link>
+        <Link to="">Skills</Link>
         <Link to="">Career</Link>
-        <Link to="">Help?</Link>
+        {/* <Link to="">Help?</Link> */}
       </div>
 
-      <div className='navbar-right'>
+      {/* <div className='navbar-right'>
         <Link to="/register">Onboarding</Link>
+        <Link to="">Login</Link>
         <Link to=""><img src={compare} alt='' className='navbar-bell'></img></Link>
         <Link to=""><img src={notification} alt='' className='navbar-bell'></img></Link>
         <Link to=""><img src={signout} alt='' className='navbar-bell'></img></Link>
+      </div> */}
+
+      <div className='navbar-right'>
+        {/* <Link to='/register'>Onboarding</Link> */}
+        {isLoggedIn ? (
+          <>
+            <img src={compare} alt='' className='navbar-bell' />
+            <img src={notification} alt='' className='navbar-bell' />
+            <img src={signout} alt='' className='navbar-bell' />
+            <Link to=''><img src={userData && userData.profilePicture} alt="" className='navbar-profile-picture' /></Link>
+          </>
+        ) : (
+          <>
+            <Link to='/register'>Onboarding</Link>
+            <Link to='/login'>Login</Link>
+          </>
+        )}
       </div>
+
     </div>
   )
 }
