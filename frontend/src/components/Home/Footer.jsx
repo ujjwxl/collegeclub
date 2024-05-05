49f4fc5029d7d9fcd03e0d415729d7aca2f7024b
agrawalaman775 <agrawalaman977@gmail.com>
2024-05-05 15:05:26 +0530

    commit 104
    footer

--- frontend/src/components/Home/Footer.jsx
+++ frontend/src/components/Home/Footer.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import './Footer.css';
 import { Link } from 'react-router-dom';
@@ -60,6 +61,67 @@ const Footer = () => {
             </div>
         </footer>
     );
+=======
+import React from "react";
+import "./Footer.css";
+import linkedInLogo from "../../assets/linkedin-logo.png";
+import twitterLogo from "../../assets/twitter-logo.png";
+import facebookLogo from "../../assets/facebook-logo.png";
+import instagramLogo from "../../assets/instagram-logo.png";
+import youtubeLogo from "../../assets/youtube-logo.png";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-column">
+        <h4>Company</h4>
+        <ul>
+          <li>
+            <a href="/about-us">About Us</a>
+          </li>
+          <li>Mission & Vision</li>
+          <li>Contact</li>
+          <li>Privacy</li>
+          <li>Terms</li>
+        </ul>
+      </div>
+      <div className="footer-column">
+        <h4>Account</h4>
+        <ul>
+          <li>Onboarding</li>
+          <li>Login</li>
+        </ul>
+        
+      </div>
+      <div className="footer-column">
+        <h4>Other Links</h4>
+        <ul>
+        <li>
+            <a href="/career">Careers</a>
+          </li>
+          <li>FAQs</li>
+          <li>Feedback and Suggestions</li>
+        </ul>
+      </div>
+      <div className="footer-column">
+        <h4>Contact Us</h4>
+        <p>CIMP-BIF Building,</p>
+        <p>Mithapur Farm Area,</p>
+        <p>Mithapur, Patna,</p>
+        <p>Bihar 800001 India</p>
+        <p>+91 7368071436</p>
+        <p>support@collegeclub.io</p>
+        <div className="footer-social-links">
+          <img src={linkedInLogo} alt="" />
+          <img src={twitterLogo} alt="" />
+          <img src={facebookLogo} alt="" />
+          <img src={instagramLogo} alt="" />
+          <img src={youtubeLogo} alt="" />
+        </div>
+      </div>
+    </footer>
+  );
+>>>>>>> Stashed changes
 };
 
 export default Footer;
