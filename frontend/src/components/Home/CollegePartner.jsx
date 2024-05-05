4df37e3f2161c1126e7fb324984985a4b162faff
ujjwxl <ujjwalk1408@gmail.com>
2024-05-05 12:37:45 +0530

    commit 100
    fix svg distance

--- frontend/src/components/Home/CollegePartner.jsx
+++ frontend/src/components/Home/CollegePartner.jsx
@@ -17,30 +17,30 @@ const CollegePartner = () => {
                         {/* Inner circle */}
                         <circle cx="200" cy="200" r="60" fill="none" stroke="black" strokeWidth="2" />
 
-                        <image href={collegeClubLogo} x="175" y="175" width="50" height="50" />
+                        <image href={collegeClubLogo} x="180" y="182" width="40" height="40" />
 
                         {/* Logos in outer circle */}
                         <g transform="rotate(-45 200 200)">
-                            <image href="https://upload.wikimedia.org/wikipedia/en/d/d2/Birla_Institute_of_Technology_Mesra.png" x="250" y="50" width="50" height="50" />
-                            <image href="https://upload.wikimedia.org/wikipedia/en/5/52/Indian_Institute_of_Technology%2C_Patna.svg" x="150" y="25" width="50" height="50" />
-                            <image href="https://upload.wikimedia.org/wikipedia/en/6/69/IIT_Madras_Logo.svg" x="50" y="85" width="50" height="50" />
+                            <image href="https://upload.wikimedia.org/wikipedia/en/d/d2/Birla_Institute_of_Technology_Mesra.png" x="260" y="50" width="50" height="50" />
+                            <image href="https://upload.wikimedia.org/wikipedia/en/5/52/Indian_Institute_of_Technology%2C_Patna.svg" x="150" y="30" width="50" height="50" />
+                            <image href="https://upload.wikimedia.org/wikipedia/en/6/69/IIT_Madras_Logo.svg" x="65" y="75" width="50" height="50" />
 
-                            <image href="https://upload.wikimedia.org/wikipedia/en/b/b5/National_Institute_of_Technology%2C_Patna_Logo.png" x="50" y="260" width="50" height="50" />
+                            <image href="https://upload.wikimedia.org/wikipedia/en/b/b5/National_Institute_of_Technology%2C_Patna_Logo.png" x="65" y="275" width="50" height="50" />
                             <image href="https://upload.wikimedia.org/wikipedia/en/f/fe/Srmseal.png" x="150" y="320" width="50" height="50" />
-                            <image href="https://upload.wikimedia.org/wikipedia/en/b/b5/National_Institute_of_Technology%2C_Patna_Logo.png" x="250" y="300" width="50" height="50" />
+                            <image href="https://upload.wikimedia.org/wikipedia/en/b/b5/National_Institute_of_Technology%2C_Patna_Logo.png" x="250" y="305" width="50" height="50" />
 
-                            <image href="https://upload.wikimedia.org/wikipedia/en/d/d2/Birla_Institute_of_Technology_Mesra.png" x="320" y="240" width="50" height="50" />
+                            <image href="https://upload.wikimedia.org/wikipedia/en/d/d2/Birla_Institute_of_Technology_Mesra.png" x="315" y="240" width="50" height="50" />
                             <image href="https://upload.wikimedia.org/wikipedia/en/5/52/Indian_Institute_of_Technology%2C_Patna.svg" x="320" y="140" width="50" height="50" />
-                            <image href="https://upload.wikimedia.org/wikipedia/en/6/69/IIT_Madras_Logo.svg" x="20" y="180" width="50" height="50" />
+                            <image href="https://upload.wikimedia.org/wikipedia/en/6/69/IIT_Madras_Logo.svg" x="25" y="180" width="50" height="50" />
                             {/* Add more image elements for other logos */}
                         </g>
 
                         {/* Logos in inner circle */}
                         <g transform="rotate(90 200 200)">
                             {/* <image href="https://upload.wikimedia.org/wikipedia/en/d/d2/Birla_Institute_of_Technology_Mesra.png" x="200" y="120" width="50" height="50" /> */}
-                            <image href="https://upload.wikimedia.org/wikipedia/en/5/52/Indian_Institute_of_Technology%2C_Patna.svg" x="140" y="180" width="50" height="140" />
+                            <image href="https://upload.wikimedia.org/wikipedia/en/5/52/Indian_Institute_of_Technology%2C_Patna.svg" x="150" y="180" width="50" height="140" />
                             {/* <image href="https://upload.wikimedia.org/wikipedia/en/d/d2/Birla_Institute_of_Technology_Mesra.png" x="200" y="120" width="50" height="50" /> */}
-                            <image href="https://upload.wikimedia.org/wikipedia/en/d/d2/Birla_Institute_of_Technology_Mesra.png" x="140" y="120" width="50" height="50" />
+                            <image href="https://upload.wikimedia.org/wikipedia/en/d/d2/Birla_Institute_of_Technology_Mesra.png" x="150" y="120" width="50" height="50" />
                             <image href="https://upload.wikimedia.org/wikipedia/en/6/69/IIT_Madras_Logo.svg" x="230" y="180" width="50" height="50" />
                             {/* Add more image elements for other logos */}
                         </g>
