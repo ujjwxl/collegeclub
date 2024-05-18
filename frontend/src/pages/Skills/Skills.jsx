c187efeb50c11c73e6c89c82f498ad5380217f96
agrawalaman775 <agrawalaman977@gmail.com>
2024-05-18 16:28:00 +0530

    commit 129
    responsive

--- frontend/src/pages/Skills/Skills.jsx
+++ frontend/src/pages/Skills/Skills.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
-import location from '../../assets/location.png';
+import location from "../../assets/location.png";
 import Footer from "../../components/Home/Footer";
 import backgroundImage from "../../assets/home-1.jpg";
 import backgroundImageTwo from "../../assets/home-2.jpg";
@@ -11,129 +11,195 @@ import axios from "axios";
 import "./Skills.css";
 
 const Skills = () => {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedCourse, setSelectedCourse] = useState(null);
+  const [examDetails, setExamDetails] = useState({});
+  const [isFilterModalOpen, setIsFilterModalOpen] = useState(false);
 
-    const [selectedCategory, setSelectedCategory] = useState('');
-    const [selectedCourse, setSelectedCourse] = useState(null);
-    const [examDetails, setExamDetails] = useState({});
+  const categories = ["Programming", "Soft Skills"];
 
-    const categories = ['Programming', 'Soft Skills'];
+  const courses = [
+    {
+      courseImage: backgroundImage,
+      category: "Programming",
+      courseName: "JavaScript",
+      courseInstructor: "Arvind Kumar",
+      price: "Rs 3,099",
+      id: 1,
+    },
+    {
+      courseImage: backgroundImageThree,
+      category: "Soft Skills",
+      courseName: "JavaScript",
+      courseInstructor: "Arvind Kumar",
+      price: "Rs 3,099",
+      id: 2,
+    },
+    {
+      courseImage: backgroundImage,
+      category: "Programming",
+      courseName: "JavaScript",
+      courseInstructor: "Arvind Kumar",
+      price: "Rs 3,099",
+      id: 3,
+    },
+    {
+      courseImage: backgroundImageTwo,
+      category: "Soft Skills",
+      courseName: "JavaScript",
+      courseInstructor: "Arvind Kumar",
+      price: "Rs 3,099",
+      id: 4,
+    },
+    {
+      courseImage: backgroundImageTwo,
+      category: "Programming",
+      courseName: "JavaScript",
+      courseInstructor: "Arvind Kumar",
+      price: "Rs 3,099",
+      id: 5,
+    },
+  ];
 
-    const courses = [
-        {
-            courseImage: backgroundImage,
-            category: 'Programming',
-            courseName: 'JavaScript',
-            courseInstructor: 'Arvind Kumar',
-            price: 'Rs 3,099',
-            id: 1
-        },
-        {
-            courseImage: backgroundImageThree,
-            category: 'Soft Skills',
-            courseName: 'JavaScript',
-            courseInstructor: 'Arvind Kumar',
-            price: 'Rs 3,099',
-            id: 2
-        },
-        {
-            courseImage: backgroundImage,
-            category: 'Programming',
-            courseName: 'JavaScript',
-            courseInstructor: 'Arvind Kumar',
-            price: 'Rs 3,099',
-            id: 3
-        },
-        {
-            courseImage: backgroundImageTwo,
-            category: 'Soft Skills',
-            courseName: 'JavaScript',
-            courseInstructor: 'Arvind Kumar',
-            price: 'Rs 3,099',
-            id: 4
-        },
-        {
-            courseImage: backgroundImageTwo,
-            category: 'Programming',
-            courseName: 'JavaScript',
-            courseInstructor: 'Arvind Kumar',
-            price: 'Rs 3,099',
-            id: 5
-        }
-    ];
+  const filteredCourses = selectedCategory
+    ? courses.filter((course) => course.category === selectedCategory)
+    : courses;
 
-    const filteredCourses = selectedCategory
-        ? courses.filter(course => course.category === selectedCategory)
-        : courses;
+  const groupCoursesIntoRows = () => {
+    const rows = [];
+    for (let i = 0; i < filteredCourses.length; i += 3) {
+      rows.push(filteredCourses.slice(i, i + 3));
+    }
+    return rows;
+  };
 
-    const groupCoursesIntoRows = () => {
-        const rows = [];
-        for (let i = 0; i < filteredCourses.length; i += 3) {
-            rows.push(filteredCourses.slice(i, i + 3));
-        }
-        return rows;
-    };
+  const handleViewMore = (course) => {
+    console.log(course);
+    setSelectedCourse(course);
+  };
 
-    const handleViewMore = (course) => {
-        console.log(course);
-        setSelectedCourse(course);
-    };
+  const handleCloseModal = () => {
+    setSelectedCourse(null);
+  };
+  const toggleFilterModal = () => {
+    setIsFilterModalOpen(!isFilterModalOpen);
+  };
 
-    const handleCloseModal = () => {
-        setSelectedCourse(null);
-    };
-
-    return (
-        <>
-            <Navbar />
-            <img src={backgroundImage} alt="" className="home1-img" />
-            <div className="skills-overlay colleges-container">
-                <div className="skills-display-box">
-
-                    <div className="skills-display-box-filter">
-                        <h3>Select a category</h3>
-
-                        <div className={`skills-left-items ${selectedCategory === '' ? 'skills-selected-button' : ''}`} onClick={() => setSelectedCategory('')}>
-                            <h3>All</h3>
-                        </div>
-
-                        {categories.map((course, index) => (
-                            <div className={`skills-left-items ${selectedCategory === course ? 'skills-selected-button' : ''}`} key={index} onClick={() => setSelectedCategory(course)}>
-                                <h3>{course}</h3>
-                            </div>
-                        ))}
-
-                    </div>
-
-                    <div className="skills-display-box-list">
-                        <h1>Courses</h1>
-                        {groupCoursesIntoRows().map((row, rowIndex) => (
-                            <div className="skills-cards-row" key={rowIndex}>
-                                {row.map((course, index) => (
-                                    <div className="skills-course-card" key={index}>
-                                        <img src={course.courseImage} alt="" />
-                                        <img className="skill-course-card-image" src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" alt="" />
-                                        <p>{course.courseName}</p>
-                                        <p>{course.courseInstructor}</p>
-                                        <p>{course.price}</p>
-                                        <button className="skill-course-card-button" onClick={() => handleViewMore(course)}>View more</button>
-                                    </div>
-                                ))}
-                            </div>
-                        ))}
-                    </div>
-                </div>
+  return (
+    <>
+      <Navbar />
+      <img src={backgroundImage} alt="" className="home1-img" />
+      <div className="skills-overlay colleges-container">
+        <div className="skills-title">
+          <h2>Courses</h2>
+          <button className="colleges-filter-btn" onClick={toggleFilterModal}>
+            FILTER
+          </button>
+        </div>
+        {isFilterModalOpen && (
+          <div className="filter-modal">
+            <div className="filter-modal-header">
+              <h2>Courses Type</h2>
+              <div className="clear-apply">
+                <button
+                  className="colleges-filter-modal-btn"
+                  onClick={toggleFilterModal}
+                >
+                  APPLY
+                </button>
+              </div>
             </div>
-            {selectedCourse && (
-                <div className="modal">
-                    <div className="modal-content">
-                        <p>ID : {selectedCourse.id}</p>
-                        <button className="close-button" onClick={handleCloseModal}>Close</button>
-                    </div>
+            <div className="filter-modal-content-courses">
+              <div
+                className={`skills-left-items ${
+                  selectedCategory === "" ? "skills-selected-button" : ""
+                }`}
+                onClick={() => setSelectedCategory("")}
+              >
+                <h3>All</h3>
+              </div>
+
+              {categories.map((course, index) => (
+                <div
+                  className={`skills-left-items ${
+                    selectedCategory === course ? "skills-selected-button" : ""
+                  }`}
+                  key={index}
+                  onClick={() => setSelectedCategory(course)}
+                >
+                  <h3>{course}</h3>
                 </div>
-            )}
-            <Footer />
-        </>
-    );
+              ))}
+            </div>
+          </div>
+        )}
+        <div className="skills-display-box">
+          <div className="skills-display-box-filter">
+            <h3>Select a category</h3>
+
+            <div
+              className={`skills-left-items ${
+                selectedCategory === "" ? "skills-selected-button" : ""
+              }`}
+              onClick={() => setSelectedCategory("")}
+            >
+              <h3>All</h3>
+            </div>
+
+            {categories.map((course, index) => (
+              <div
+                className={`skills-left-items ${
+                  selectedCategory === course ? "skills-selected-button" : ""
+                }`}
+                key={index}
+                onClick={() => setSelectedCategory(course)}
+              >
+                <h3>{course}</h3>
+              </div>
+            ))}
+          </div>
+
+          <div className="skills-display-box-list">
+            <h1 className="courses-skills">Courses</h1>
+            {groupCoursesIntoRows().map((row, rowIndex) => (
+              <div className="skills-cards-row" key={rowIndex}>
+                {row.map((course, index) => (
+                  <div className="skills-course-card" key={index}>
+                    <img
+                      src={course.courseImage}
+                      alt=""
+                      className="skills-course-card-img"
+                    />
+                    {/* <img className="skill-course-card-image" src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" alt="" /> */}
+                    <h3>{course.courseName}</h3>
+                    <p className="instructor">{course.courseInstructor}</p>
+                    <p>{course.price}</p>
+                    <button
+                      className="college-button"
+                      onClick={() => handleViewMore(course)}
+                    >
+                      View more
+                    </button>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      {selectedCourse && (
+        <div className="modal">
+          <div className="modal-content">
+            <p>ID : {selectedCourse.id}</p>
+            <button className="close-button" onClick={handleCloseModal}>
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+      <Footer />
+    </>
+  );
 };
 
 export default Skills;
