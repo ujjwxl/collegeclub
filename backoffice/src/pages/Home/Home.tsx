825c1939d58bed46533bb63328c22fcbe2e69e55
agrawalaman775 <agrawalaman977@gmail.com>
2024-06-21 15:22:03 +0530

    commit 198
    dashboard

--- backoffice/src/pages/Home/Home.tsx
+++ backoffice/src/pages/Home/Home.tsx
@@ -10,6 +10,13 @@ import employee from "../../assets/employee.png";
 import settings from "../../assets/settings.png";
 import help from "../../assets/help.png";
 import application from "../../assets/application.png";
+import partners from "../../assets/partners.png";
+import onboarding from "../../assets/onboarding.png";
+import envelope from "../../assets/envelope.png";
+import book from "../../assets/book.png";
+import agreement from "../../assets/agreement.png";
+import chat from "../../assets/chat.png";
+import event from "../../assets/event.png";
 
 const Home: React.FC = () => {
     return (
@@ -32,46 +39,79 @@ const Home: React.FC = () => {
                     />
                 </div>
             </div>
-            <div className="flex h-screen">
-                <div className="bg-slate-200 flex-col p-6 px-10 pt-2">
+            <div className="flex h-screen ">
+                <div className="bg-slate-200 flex-col w-1/6 pt-2">
                     <div className=" flex flex-col items-center justify-center my-3">
                         <img src={home} className="h-8 object-contain"></img>
-                        <p>Home</p>
+                        <p className="font-semibold">Home</p>
                     </div>
                     <div className=" flex flex-col items-center justify-center my-3">
                         <img src={team} className="h-12 object-contain"></img>
-                        <p>Office Team</p>
+                        <p className="font-semibold">Office Team</p>
                     </div>
                     <div className=" flex flex-col items-center justify-center my-3">
                         <img src={enquiry} className="h-8 object-contain"></img>
-                        <p>Enquiry</p>
+                        <p className="font-semibold">Enquiry</p>
                     </div>
                     <div className=" flex flex-col items-center justify-center my-3">
                         <img src={directory} className="h-8 object-contain"></img>
-                        <p>Directory</p>
+                        <p className="font-semibold">Directory</p>
                     </div>
                     <div className=" flex flex-col items-center justify-center my-3">
                         <img src={library} className="h-8 object-contain"></img>
-                        <p>Data Library</p>
+                        <p className="font-semibold">Data Library</p>
                     </div>
                     <div className=" flex flex-col items-center justify-center my-3">
                         <img src={employee} className="h-10 object-contain"></img>
-                        <p>Employee</p>
+                        <p className="font-semibold">Employee</p>
                     </div>
                     <div className=" flex flex-col items-center justify-center my-3">
                         <img src={settings} className="h-8 object-contain"></img>
-                        <p>Control Panel</p>
+                        <p className="font-semibold">Control Panel</p>
                     </div>
                     <div className=" flex flex-col items-center justify-center my-3">
                         <img src={help} className="h-8 object-contain"></img>
-                        <p>Help</p>
-                    </div>
-                    <div className=" flex flex-col items-center justify-center my-3">
-                        <img src={application} className="h-8 object-contain"></img>
-                        <p>Tools</p>
+                        <p className="font-semibold">Help</p>
                     </div>
                 </div>
-                <div className=""></div>
+                <div className="w-5/6">
+                    <div className=" w-full flex justify-center mt-16">
+                        <div className="bg-slate-500 rounded-lg flex px-3 py-7 mx-3 w-64 items-center">
+                            <img src={partners} className="h-8 object-contain"></img>
+                            <p className="ml-3 font-semibold">Partners</p>
+                        </div>
+                        <div className="bg-slate-500 rounded-lg flex px-3 py-7 mx-3 w-64 items-center">
+                            <img src={onboarding} className="h-8 object-contain"></img>
+                            <p className="ml-3  font-semibold">New Onboarding</p>
+                        </div>
+                        <div className="bg-slate-500 rounded-lg flex px-3 py-7 mx-3 w-64 items-center">
+                            <img src={partners} className="h-8 object-contain"></img>
+                            <p className="ml-3 font-semibold">Total Leads</p>
+                        </div>
+                        <div className="bg-slate-500 rounded-lg flex px-3 py-7 mx-3 w-64 items-center">
+                            <img src={envelope} className="h-8 object-contain"></img>
+                            <p className="ml-3 font-semibold ">Work Request</p>
+                        </div>
+                    </div>
+                    <div className=" w-full flex justify-center mt-10">
+                        <div className="bg-slate-500 rounded-lg flex px-3 py-7 mx-3 w-64 items-center">
+                            <img src={book} className="h-8 object-contain"></img>
+                            <p className="ml-3 font-semibold ">Course Enrollment</p>
+                        </div>
+                        <div className="bg-slate-500 rounded-lg flex px-3 py-7 mx-3 w-64 items-center">
+                            <img src={agreement} className="h-8 object-contain"></img>
+                            <p className="ml-3 font-semibold ">New Admission</p>
+                        </div>
+                        <div className="bg-slate-500 rounded-lg flex px-3 py-7 mx-3 w-64 items-center">
+                            <img src={chat} className="h-8 object-contain"></img>
+                            <p className="ml-3  font-semibold ">Call Request</p>
+                        </div>
+                        <div className="bg-slate-500 rounded-lg flex px-3 py-7 mx-3 w-64 items-center">
+                            <img src={event} className="h-8 object-contain"></img>
+                            <p className="ml-3 font-semibold  ">Events</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
