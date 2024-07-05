7e877c087361da13f7f189f9741767127242bce8
ujjwxl <ujjwalk1408@gmail.com>
2024-07-05 16:41:04 +0530

    commit 250
    added search

--- backoffice/src/pages/Partners.tsx
+++ backoffice/src/pages/Partners.tsx
@@ -1,33 +1,56 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Navbar from "../components/Navbar";
-import Sidebar from "../components/Sidebar";
-import axios, { AxiosError, AxiosResponse } from "axios";
+import React, { useState, useEffect, ChangeEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+
+interface Partner {
+  userId: string;
+  organizationName: string;
+  email: string;
+  contactNumber: string;
+  accountType: string;
+  paymentStatus: boolean;
+}
 
 const Partners = () => {
-  const [partners, setPartners] = useState<any[]>([]);
+  const [partners, setPartners] = useState<Partner[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [searchInput, setSearchInput] = useState('');
 
   const navigate = useNavigate();
 
+  useEffect(() => {
+    getPartners('College');
+  }, []);
+
   const getPartners = async (partnerType: string) => {
-    await axios
-      .get(`http://localhost:5000/admin/getpartners/${partnerType}`)
-      .then((response: AxiosResponse) => {
-        setPartners(response.data);
-        console.log(response.data);
-      })
-      .catch((error: AxiosError) => {
-        alert("Could not get partners");
-        console.log(error);
-      });
+    try {
+      const response: AxiosResponse<Partner[]> = await axios.get(`http://localhost:5000/admin/getpartners/${partnerType}`);
+      setPartners(response.data);
+      setLoading(false);
+    } catch (error: any) {
+      alert('Could not get partners');
+      console.error('Error fetching partners:', error.message);
+      setLoading(false);
+    }
   };
 
   const handleShowDetails = (partnerType: string, userId: string) => {
-    if (partnerType === "College") {
+    if (partnerType === 'College') {
       navigate(`/college/${userId}`);
-    } else if(partnerType === "Company") {
+    } else if (partnerType === 'Company') {
       navigate(`/company/${userId}`);
     }
+  };
+
+  const filteredPartners = partners.filter(partner =>
+    partner.email.includes(searchInput) ||
+    partner.contactNumber.includes(searchInput)
+  );
+
+  const handleSearchInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(event.target.value);
   };
 
   return (
@@ -37,121 +60,98 @@ const Partners = () => {
         <Sidebar />
         <div className="w-5/6">
           <div className="flex w-full justify-around">
-            <div
-              className="w-1/5 bg-slate-500 mt-4 p-12 text-center rounded-xl"
-              onClick={() => getPartners("College")}
-            >
+            <div className="w-1/5 bg-slate-500 mt-4 p-12 text-center rounded-xl" onClick={() => getPartners('College')}>
               <p className="text-xl text-white">College</p>
             </div>
-
-            <div
-              className="w-1/5 bg-slate-500 mt-4 p-12 text-center rounded-xl"
-              onClick={() => getPartners("Company")}
-            >
+            <div className="w-1/5 bg-slate-500 mt-4 p-12 text-center rounded-xl" onClick={() => getPartners('Company')}>
               <p className="text-xl text-white">Company</p>
             </div>
-
-            <div
-              className="w-1/5 bg-slate-500 mt-4 p-12 text-center rounded-xl"
-              onClick={() => getPartners("CC-Ambassador")}
-            >
+            <div className="w-1/5 bg-slate-500 mt-4 p-12 text-center rounded-xl" onClick={() => getPartners('CC-Ambassador')}>
               <p className="text-xl text-white">CC-Ambassador</p>
             </div>
           </div>
 
-          {partners && partners.length > 0 ? (
-            <div className="m-4">
-              <table className="min-w-full divide-y divide-gray-200 college-table">
-                <thead className="bg-gray-400">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
-                    >
-                      S.No.
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
-                    >
-                      Name
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
-                    >
-                      Email
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
-                    >
-                      Phone
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
-                    >
-                      Payment Status
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
-                    >
-                      Details
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="bg-gray-100 divide-y divide-gray-200">
-                  {partners.map((partner, index) => (
-                    <tr key={partner.userId}>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="text-sm font-medium text-gray-900">
-                          {index + 1}
-                        </div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="text-sm font-medium text-gray-900">
-                          {partner.organizationName}
-                        </div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="text-sm text-gray-900">
-                          {partner.email}
-                        </div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="text-sm text-gray-900">
-                          {partner.contactNumber}
-                        </div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="text-sm text-gray-900">
-                          {partner.paymentStatus
-                            ? "Completed"
-                            : "Not Completed"}
-                        </div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        <button
-                          className="bg-gray-500 px-2 py-1 text-sm rounded-lg text-white"
-                          onClick={() =>
-                            handleShowDetails(
-                              partner.accountType,
-                              partner.userId
-                            )
-                          }
-                        >
-                          View more
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+          {loading ? (
+            <p className="m-4">Loading...</p>
           ) : (
-            <p>No partners available</p>
+            <div className="m-4">
+              <input
+                type="text"
+                value={searchInput}
+                onChange={handleSearchInputChange}
+                placeholder="Search by Email or Phone..."
+                className="border border-gray-300 w-1/3 rounded-lg px-3 py-1 mb-4"
+              />
+              {filteredPartners.length > 0 ? (
+                <div className="overflow-x-auto">
+                  <table className="min-w-full divide-y divide-gray-200 college-table">
+                    <thead className="bg-gray-400">
+                      <tr>
+                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
+                          S.No.
+                        </th>
+                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
+                          Name
+                        </th>
+                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
+                          Email
+                        </th>
+                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
+                          Phone
+                        </th>
+                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
+                          Payment Status
+                        </th>
+                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
+                          Details
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody className="bg-gray-100 divide-y divide-gray-200">
+                      {filteredPartners.map((partner, index) => (
+                        <tr key={partner.userId}>
+                          <td className="px-6 py-4 whitespace-nowrap">
+                            <div className="text-sm font-medium text-gray-900">
+                              {index + 1}
+                            </div>
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap">
+                            <div className="text-sm font-medium text-gray-900">
+                              {partner.organizationName}
+                            </div>
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap">
+                            <div className="text-sm text-gray-900">
+                              {partner.email}
+                            </div>
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap">
+                            <div className="text-sm text-gray-900">
+                              {partner.contactNumber}
+                            </div>
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap">
+                            <div className="text-sm text-gray-900">
+                              {partner.paymentStatus ? 'Completed' : 'Not Completed'}
+                            </div>
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap">
+                            <button
+                              className="bg-gray-500 px-2 py-1 text-sm rounded-lg text-white"
+                              onClick={() => handleShowDetails(partner.accountType, partner.userId)}
+                            >
+                              View more
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              ) : (
+                <p>No partners found</p>
+              )}
+            </div>
           )}
         </div>
       </div>
--- backoffice/src/pages/Users.tsx
+++ backoffice/src/pages/Users.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
-import axios, { AxiosError, AxiosResponse } from 'axios';
+import axios, { AxiosResponse, AxiosError } from 'axios';
 
 interface User {
     userId: string;
@@ -13,6 +13,8 @@ interface User {
 
 function Users(): JSX.Element {
     const [users, setUsers] = useState<User[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [searchInput, setSearchInput] = useState('');
 
     useEffect(() => {
         getUsers();
@@ -22,10 +24,23 @@ function Users(): JSX.Element {
         try {
             const response: AxiosResponse<User[]> = await axios.get('http://localhost:5000/admin/getAllUsers');
             setUsers(response.data);
+            setLoading(false);
         } catch (error: any) {
             alert('Could not fetch users');
-            console.error(error);
+            console.error('Error fetching users:', error.message);
+            setLoading(false);
         }
+    };
+
+    // Filter users based on search input
+    const filteredUsers = users.filter(user =>
+        user.email.includes(searchInput) ||
+        user.contactNumber.includes(searchInput)
+    );
+
+    // Handle input change for search
+    const handleSearchInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setSearchInput(event.target.value);
     };
 
     return (
@@ -36,63 +51,73 @@ function Users(): JSX.Element {
 
                 <div className="w-5/6">
                     <h1 className='font-semibold text-4xl m-4 mb-8'>Users</h1>
-                    {users && users.length > 0 ? (
-                        <div className="m-4">
-                            <div className="overflow-x-auto">
-                                <table className="min-w-full divide-y divide-gray-200 shadow-md border border-gray-200 rounded-lg">
-                                    <thead className="bg-gray-100">
-                                        <tr>
-                                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">
-                                                S.No.
-                                            </th>
-                                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">
-                                                Name
-                                            </th>
-                                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">
-                                                Email
-                                            </th>
-                                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">
-                                                Phone
-                                            </th>
-                                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">
-                                                Role
-                                            </th>
-                                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">
-                                                UserID
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody className="bg-slate-50 divide-y divide-gray-200">
-                                        {users.map((user, index) => (
-                                            <tr key={user.userId} className="transition-all hover:bg-slate-100">
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                                    {index + 1}
-                                                </td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                                                    {user.organizationName}
-                                                </td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                                    {user.email}
-                                                </td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                                    {user.contactNumber}
-                                                </td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                                    {user.accountType}
-                                                </td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                                    {user.userId}
-                                                </td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+                    {loading ? (
+                        <p className="m-4">Loading...</p>
                     ) : (
-                        <p className="m-4">No users available</p>
+                        <div className="m-4">
+                            <input
+                                type="text"
+                                value={searchInput}
+                                onChange={handleSearchInputChange}
+                                placeholder="Search by Email or Phone..."
+                                className="border border-gray-300 w-1/3 rounded-lg px-3 py-1 mb-4"
+                            />
+                            {filteredUsers.length > 0 ? (
+                                <div className="overflow-x-auto">
+                                    <table className="min-w-full divide-y divide-gray-200 shadow-md border border-gray-200 rounded-lg">
+                                        <thead className="bg-gray-100">
+                                            <tr>
+                                                <th className="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">
+                                                    S.No.
+                                                </th>
+                                                <th className="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">
+                                                    Name
+                                                </th>
+                                                <th className="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">
+                                                    Email
+                                                </th>
+                                                <th className="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">
+                                                    Phone
+                                                </th>
+                                                <th className="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">
+                                                    Role
+                                                </th>
+                                                <th className="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">
+                                                    UserID
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody className="bg-slate-50 divide-y divide-gray-200">
+                                            {filteredUsers.map((user, index) => (
+                                                <tr key={user.userId} className="transition-all hover:bg-slate-100">
+                                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                                        {index + 1}
+                                                    </td>
+                                                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                                        {user.organizationName}
+                                                    </td>
+                                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                                        {user.email}
+                                                    </td>
+                                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                                        {user.contactNumber}
+                                                    </td>
+                                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                                        {user.accountType}
+                                                    </td>
+                                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                                        {user.userId}
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                </div>
+                            ) : (
+                                <p className="m-4">No users found</p>
+                            )}
+                        </div>
                     )}
-
                 </div>
             </div>
         </div>
